d27cbe4f93ccbcec6f3fa389b7327c5b9bf26ced
NJTAKAYUKI <nj.takayuki@gmail.com>
2019-01-08 17:32:51 +0000

    バリデーションうまくいった

--- cms/app/Http/Controllers/Npo_registerController.php
+++ cms/app/Http/Controllers/Npo_registerController.php
@@ -133,7 +133,7 @@ class Npo_registerController extends Controller {
 		$rules = [
 		  //  'npo_name' => 'required|unique:npo_registers,npo_name',
 		    'title' => 'required | min:1 | max:55',
-		    'member1' => 'unique:users,name',
+		  //  'member1' => 'unique:users,name',
 		];
 // 		\Validator::make($npo_register, [
 //             'member1' => [
@@ -159,104 +159,32 @@ class Npo_registerController extends Controller {
         $npo_register->yellow_card_title = $request->input("yellow_card_title");
         $npo_register->yellow_card_body  = $request->input("yellow_card_body");
         
-        // 1~10までの処理を、本当はfor文で回したいけど、うまくいかない。
-        // for($i = 1; $i < 11; $i++){
-        //     $i_pos = $i."_pos";
-        //     $i_detail = $i."_detail";
-        //     $i_twitter = $i."_twitter";
-        //     $i_facebook = $i."_facebook";
-        //     $i_linkedin = $i."_linkedin";
-            // dd("member{$i}");
-            // $npo_register->member.$i      = $request->input("member{$i}");
-            // $npo_register->member.$i_pos      = $request->input("member{$i_pos}");
-            // dd($npo_register->member.$i_pos);
-            // $npo_register->member.$i_detail = $request->input("member{$i_detail}");
-            // $npo_register->member.$i_twitter  = $request->input("member{$i_twitter}");
-            // $npo_register->member.$i_facebook = $request->input("member{$i_facebook}");
-            // $npo_register->member.$i_linkedin  = $request->input("member{$i_linkedin}");
-            // if($i==3){
-            //     dd($npo_register->member.$i_pos);
-            // }
-        // }
-        $currentUserInfo = \DB::table('users')->where('name', $request->input("member1"))->first();
-        if($currentUserInfo){
-            if($currentUserInfo->name == $request->input("member1")){
-                $npo_register->member1 = $request->input("member1");
-                $npo_register->member1_pos = $request->input("member1_pos");
-                $npo_register->member1_detail = $request->input("member1_detail");
-                $npo_register->member1_twitter = $request->input("member1_twitter");
-                $npo_register->member1_facebook = $request->input("member1_facebook");
-                $npo_register->member1_linkedin = $request->input("member1_linkedin");
-                dd("a");
+        // メンバーには、サイトに登録済みの「ユーザー名」を入れてください。
+        for($i = 1; $i < 11; $i++){
+            $member          = "member".$i;
+            $member_pos      = $member."_pos";
+            $member_detail   = $member."_detail";
+            $member_twitter  = $member."_twitter";
+            $member_facebook = $member."_facebook";
+            $member_linkedin = $member."_linkedin";
+            $currentUserInfo = \DB::table('users')->where('name', $request->input($member))->first();
+            // dd($currentUserInfo);
+            if($currentUserInfo){
+                if($currentUserInfo->name == $request->input($member)){
+                    $npo_register->$member          = $request->input($member);
+                    $npo_register->$member_pos      = $request->input($member_pos);
+                    $npo_register->$member_detail   = $request->input($member_detail);
+                    $npo_register->$member_twitter  = $request->input($member_twitter);
+                    $npo_register->$member_facebook = $request->input($member_facebook);
+                    $npo_register->$member_linkedin = $request->input($member_linkedin);
+                }
+            }else{
+                \Validator::make(
+                    [$member => $request[$member]],
+                    [$member => 'mimes']
+                )->validate();
             }
         }
-        
-        back();
-        dd($currentUserInfo);
-        dd("stop");
-        
-        $npo_register->member2 = $request->input("member2");
-        $npo_register->member2_pos = $request->input("member2_pos");
-        $npo_register->member2_detail = $request->input("member2_detail");
-        $npo_register->member2_twitter = $request->input("member2_twitter");
-        $npo_register->member2_facebook = $request->input("member2_facebook");
-        $npo_register->member2_linkedin = $request->input("member2_linkedin");
-        
-        $npo_register->member3 = $request->input("member3");
-        $npo_register->member3_pos = $request->input("member3_pos");
-        $npo_register->member3_detail = $request->input("member3_detail");
-        $npo_register->member3_twitter = $request->input("member3_twitter");
-        $npo_register->member3_facebook = $request->input("member3_facebook");
-        $npo_register->member3_linkedin = $request->input("member3_linkedin");
-        
-        $npo_register->member4 = $request->input("member4");
-        $npo_register->member4_pos = $request->input("member4_pos");
-        $npo_register->member4_detail = $request->input("member4_detail");
-        $npo_register->member4_twitter = $request->input("member4_twitter");
-        $npo_register->member4_facebook = $request->input("member4_facebook");
-        $npo_register->member4_linkedin = $request->input("member4_linkedin");
-        
-        $npo_register->member5 = $request->input("member5");
-        $npo_register->member5_pos = $request->input("member5_pos");
-        $npo_register->member5_detail = $request->input("member5_detail");
-        $npo_register->member5_twitter = $request->input("member5_twitter");
-        $npo_register->member5_facebook = $request->input("member5_facebook");
-        $npo_register->member5_linkedin = $request->input("member5_linkedin");
-        
-        $npo_register->member6 = $request->input("member6");
-        $npo_register->member6_pos = $request->input("member6_pos");
-        $npo_register->member6_detail = $request->input("member6_detail");
-        $npo_register->member6_twitter = $request->input("member6_twitter");
-        $npo_register->member6_facebook = $request->input("member6_facebook");
-        $npo_register->member6_linkedin = $request->input("member6_linkedin");
-        
-        $npo_register->member7 = $request->input("member7");
-        $npo_register->member7_pos = $request->input("member7_pos");
-        $npo_register->member7_detail = $request->input("member7_detail");
-        $npo_register->member7_twitter = $request->input("member7_twitter");
-        $npo_register->member7_facebook = $request->input("member7_facebook");
-        $npo_register->member7_linkedin = $request->input("member7_youtube");
-        
-        $npo_register->member8 = $request->input("member8");
-        $npo_register->member8_pos = $request->input("member8_pos");
-        $npo_register->member8_detail = $request->input("member8_detail");
-        $npo_register->member8_twitter = $request->input("member8_twitter");
-        $npo_register->member8_facebook = $request->input("member8_facebook");
-        $npo_register->member8_linkedin = $request->input("member8_linkedin");
-        
-        $npo_register->member9 = $request->input("member9");
-        $npo_register->member9_pos = $request->input("member9_pos");
-        $npo_register->member9_detail = $request->input("member9_detail");
-        $npo_register->member9_twitter = $request->input("member9_twitter");
-        $npo_register->member9_facebook = $request->input("member9_facebook");
-        $npo_register->member9_linkedin = $request->input("member9_linkedin");
-        
-        $npo_register->member10 = $request->input("member10");
-        $npo_register->member10_pos = $request->input("member10_pos");
-        $npo_register->member10_detail = $request->input("member10_detail");
-        $npo_register->member10_twitter = $request->input("member10_twitter");
-        $npo_register->member10_facebook = $request->input("member10_facebook");
-        $npo_register->member10_linkedin = $request->input("member10_linkedin");
         
         $npo_register->support_purpose = $request->input("support_purpose");
         $npo_register->support_contents = $request->input("support_contents");
