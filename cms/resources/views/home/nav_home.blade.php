f07b890e59efabf4a485bffbf1240c2dc3b120f0
NJTAKAYUKI <nj.takayuki@gmail.com>
2019-03-21 14:49:15 +0000

    nav変更

--- cms/resources/views/home/nav_home.blade.php
+++ cms/resources/views/home/nav_home.blade.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="navbar-translate">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="{{ url('/') }}">F♯ BETA</a>
+                    <a class="navbar-brand" href="{{ url('/') }}">F♯</a>
                 </div>
         
                 <button class="navbar-toggler navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -62,7 +62,6 @@
                                 <a class="dropdown-item" href="{{ url('/npo_registers') }}"><i class="nc-icon nc-money-coins"></i>&nbsp; {{ Auth::user()->npo }}</a>
                         @endif        		
                                 <a class="dropdown-item" href="{{ url('home/home_own_timeline') }}"><i class="nc-icon nc-badge"></i>&nbsp; @lang('app.mypage')</a>
-                                <a class="dropdown-item" href="{{ url('home/home_register') }}"><i class="nc-icon nc-ruler-pencil"></i>&nbsp; @lang('app.mypage edit')</a>
                                 <a class="dropdown-item" href="{{ url('/logout') }}" onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();"><i class="nc-icon nc-spaceship"></i>&nbsp; @lang('app.logout')</a>
                                 <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
